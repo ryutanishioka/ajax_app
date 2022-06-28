7d0296a88582f9dcc5b73e81e31895cb620f0bd1
ryutanishioka <105966340+ryutanishioka@users.noreply.github.com>
2022-06-28 18:17:13 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,15 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.order(id: "DESC")
-
   end
 
   #def new
   #end
 
+
   def create
     Post.create(content: params[:content])
-    redirect_to action: :index
+    redirect_to action: :index 
   end
+
 end
